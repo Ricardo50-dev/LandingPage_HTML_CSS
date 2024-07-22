062991c90a9c5d64643f3d48882b33bf40d55aab
Tweedy <ricardocrvg50@gmail.com>
2024-07-22 11:00:24 -0300

    Primeiro commit

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+
+if(mobileNav){
+    mobileNav.addEventListener("click", () => toggleNav());
+}
